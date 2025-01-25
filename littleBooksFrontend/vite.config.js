521170199ac4805b7f5a41dcc5a7b4644413a50e
naylapalaoro <nayluclash@gmail.com>
2025-01-25 08:38:28 -0300

    ..

--- littleBooksFrontend/vite.config.js
+++ littleBooksFrontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/S20-12-WEBAPP/',
+  base: 'No-Country-simulation/s20-12-webapp/',
 })
 
