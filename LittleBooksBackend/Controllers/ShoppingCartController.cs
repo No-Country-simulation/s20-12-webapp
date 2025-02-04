dd53827e1bf73cf714294b113a34ba88866aa3fe
Rixda <rixda@hotmail.com.ar>
2025-02-04 15:03:49 -0300

    shoppincard
    test con front

---
+++ LittleBooksBackend/Controllers/ShoppingCartController.cs
@@ -0,0 +1,29 @@
+﻿using LittleBooksBackend.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleBooksBackend.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ShoppingCartController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult SendCart([FromBody] CartDTO request)
+        {
+
+            if (string.IsNullOrWhiteSpace(request.Nombre) || string.IsNullOrWhiteSpace(request.Email))
+            {
+                return BadRequest("Nombre y Email son requeridos.");
+            }
+
+            if (request.Books == null || request.Books.Count == 0)
+            {
+                return BadRequest("Debe proporcionar al menos un libro.");
+            }
+
+            return StatusCode(202, "Petición procesada correctamente.");
+        }
+    }
+}
---
+++ LittleBooksBackend/Models/Dtos/CartBookDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LittleBooksBackend.Models.Dtos
+{
+    public class CartBookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int Cant { get; set; }
+    }
+}
